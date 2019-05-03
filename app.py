71ea75eea072d0ef97cc2cb12fb318c363a17c21
shaheerakber67 <49983072+shaheerakber67@users.noreply.github.com>
2019-05-03 17:47:05 +0500

    Update app.py

--- app.py
+++ app.py
@@ -1,8 +1,28 @@
-#!/usr/bin/env python
+# -*- coding:utf8 -*-
+# !/usr/bin/env python
+# Copyright 2017 Google Inc. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+
+# Updated in July 2018 to reflect Dialogflow v2 changes for request/response
+# Author - Naresh Ganatra
+# http://youtube.com/c/NareshGanatra
+
 
 from __future__ import print_function
-from future.standard_library import install_aliases
-install_aliases()
+#from future.standard_library import install_aliases
+#install_aliases()
 
 from urllib.parse import urlparse, urlencode
 from urllib.request import urlopen, Request
@@ -24,42 +44,55 @@ def webhook():
     req = request.get_json(silent=True, force=True)
 
     print("Request:")
+    # commented out by Naresh
     print(json.dumps(req, indent=4))
 
     res = processRequest(req)
 
     res = json.dumps(res, indent=4)
-    # print(res)
+    #print(res)
     r = make_response(res)
     r.headers['Content-Type'] = 'application/json'
     return r
 
 
 def processRequest(req):
-    if req.get("result").get("action") != "yahooWeatherForecast":
+    print ("here I am....")
+    print ("starting processRequest...",req.get("queryResult").get("action"))
+    if req.get("queryResult").get("action") != "yahooWeatherForecast":
+        print ("Please check your action name in DialogFlow...")
         return {}
+    print("111111111111")
     baseurl = "https://query.yahooapis.com/v1/public/yql?"
+    print("1.5 1.5 1.5")
     yql_query = makeYqlQuery(req)
+    print ("2222222222")
     if yql_query is None:
         return {}
     yql_url = baseurl + urlencode({'q': yql_query}) + "&format=json"
+    print("3333333333")
+    print (yql_url)
     result = urlopen(yql_url).read()
     data = json.loads(result)
+    #for some the line above gives an error and hence decoding to utf-8 might help
+    #data = json.loads(result.decode('utf-8'))
+    print("44444444444")
+    print (data)
     res = makeWebhookResult(data)
     return res
 
 
 def makeYqlQuery(req):
-    result = req.get("result")
+    result = req.get("queryResult")
     parameters = result.get("parameters")
     city = parameters.get("geo-city")
     if city is None:
         return None
-
     return "select * from weather.forecast where woeid in (select woeid from geo.places(1) where text='" + city + "')"
 
 
 def makeWebhookResult(data):
+    print ("starting makeWebhookResult...")
     query = data.get('query')
     if query is None:
         return {}
@@ -84,24 +117,50 @@ def makeWebhookResult(data):
 
     # print(json.dumps(item, indent=4))
 
-    speech = "Today in " + location.get('city') + ": " + condition.get('text') + \
-             ", the temperature is " + condition.get('temp') + " " + units.get('temperature')
+    speech = "Today the weather in " + location.get('city') + ": " + condition.get('text') + \
+             ", And the temperature is " + condition.get('temp') + " " + units.get('temperature')
 
     print("Response:")
     print(speech)
-
+    #Naresh
     return {
-        "speech": speech,
-        "displayText": speech,
-        # "data": data,
-        # "contextOut": [],
-        "source": "apiai-weather-webhook-sample"
+
+    "fulfillmentText": speech,
+     "source": "Yahoo Weather"
     }
 
 
+@app.route('/test', methods=['GET'])
+def test():
+    return  "Hello there my friend !!"
+
+
+@app.route('/static_reply', methods=['POST'])
+def static_reply():
+    speech = "Hello there, this reply is from the webhook !! "
+    string = "You are awesome !!"
+    Message ="this is the message"
+
+    my_result =  {
+
+    "fulfillmentText": string,
+     "source": string
+    }
+
+    res = json.dumps(my_result, indent=4)
+
+    r = make_response(res)
+
+    r.headers['Content-Type'] = 'application/json'
+    return r
+
+
+
 if __name__ == '__main__':
+
+
     port = int(os.getenv('PORT', 5000))
 
     print("Starting app on port %d" % port)
 
-    app.run(debug=False, port=port, host='0.0.0.0')
+    app.run(debug=True, port=port, host='0.0.0.0')
